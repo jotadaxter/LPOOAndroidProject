37a6f9c431dbba04ec72b9a37943827b4adc33bd
jotadaxter <jotadaxter@gmail.com>
2017-05-30 00:18:36 +0100

    test configs

---
+++ desktop/src/test/java/com/mygdx/game/desktop/Test1.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop;
+
+import com.mygdx.game.MyGame;
+
+import org.junit.Test;
+
+/**
+ * Created by Utilizador on 29-05-2017.
+ */
+
+public class Test1 extends GameTest {
+    @Test
+    public void testHandler() {
+        MyGame game= new MyGame();
+    }
+}
